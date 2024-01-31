081b4415056a6fb94f81c7f11c09460ffb8f5aef
Will <pengularity@gmail.com>
2024-01-31 20:10:40 +0100

    exit status a test

--- src/exec/exec_pipeline.c
+++ src/exec/exec_pipeline.c
@@ -6,13 +6,13 @@
 /*   By: wnguyen <wnguyen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 18:04:50 by wnguyen           #+#    #+#             */
-/*   Updated: 2024/01/31 19:04:31 by wnguyen          ###   ########.fr       */
+/*   Updated: 2024/01/31 20:10:10 by wnguyen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static int    init_pipe_fds(int *pipe_fds, t_node *node)
+static int	init_pipe_fds(int *pipe_fds, t_node *node)
 {
 	if (node->next != NULL)
 	{
@@ -30,82 +30,78 @@ static int    init_pipe_fds(int *pipe_fds, t_node *node)
 	return (EXIT_SUCCESS);
 }
 
-static void    manage_parent_process(int *in_fd, int *pipe_fds,
-	t_node *current_node)
+void	finalize_pipeline(int last_pid, t_node **node, int *in_fd, t_env *env)
 {
-	if (*in_fd != STDIN_FILENO && *in_fd != -1)
+	int	status;
+
+	if (*in_fd != -1)
 		close(*in_fd);
-	if (current_node->next != NULL)
+	if (last_pid != -1)
 	{
-		if (pipe_fds[0] != -1)
-			close(pipe_fds[1]);
-		*in_fd = pipe_fds[0];
+		waitpid(last_pid, &status, 0);
+		if (WIFEXITED(status))
+			env->lst_exit = WEXITSTATUS(status);
 	}
-	else
-	{
-		if (pipe_fds[1] != -1)
-			close(pipe_fds[1]);
-		if (pipe_fds[0] != -1)
-			close(pipe_fds[0]);
-		*in_fd = -1;
-	}
+	free_nodes(*node);
 }
 
-static void    init_pids_array(int *pids, int max_cmds)
+void	exec_pipeline(t_node *node, t_env *env)
 {
-	int    index;
-
-	index = 0;
-	while (index < max_cmds)
-	{
-		pids[index] = -1;
-		index++;
-	}
-}
-
-static void    wait_for_children(int *pids, int num_cmds)
-{
-	int    index;
-	int    status;
-
-	index = 0;
-	while (index < num_cmds)
-	{
-		if (pids[index] > 0)
-			waitpid(pids[index], &status, 0);
-		index++;
-	}
-}
-
-void    exec_pipeline(t_node *node, t_env *env)
-{
-	int    pipe_fds[2];
-	int    pids[1024];
-	int    num_cmds;
-	int    in_fd;
+	int		pipe_fds[2];
+	int		last_pid;
+	int		in_fd;
 	t_node	*tmp;
 
-	num_cmds = 0;
+	last_pid = -1;
 	in_fd = STDIN_FILENO;
-	init_pids_array(pids, 1024);
-	while (node != NULL && num_cmds < 1024)
+	while (node != NULL)
 	{
 		if (init_pipe_fds(pipe_fds, node) == -1)
 			break ;
-		pids[num_cmds] = fork();
-		if (pids[num_cmds] == -1)
+		last_pid = fork();
+		if (last_pid == -1)
 			break ;
-		if (pids[num_cmds] == 0)
+		if (last_pid == 0)
 			exec_child_process(node, in_fd, env, pipe_fds);
 		else
 			manage_parent_process(&in_fd, pipe_fds, node);
 		tmp = node->next;
 		free_node(node);
 		node = tmp;
-		num_cmds++;
 	}
-	if (in_fd != -1)
-		close(in_fd);
-	wait_for_children(pids, num_cmds);
-	free_nodes(node);
+	finalize_pipeline(last_pid, &node, &in_fd, env);
 }
+
+// void    exec_pipeline(t_node *node, t_env *env)
+// {
+// 	int    pipe_fds[2];
+// 	int    pids[1024];
+// 	int    num_cmds;
+// 	int    in_fd;
+// 	t_node	*tmp;
+// 	int		child_status[1024];
+
+// 	num_cmds = 0;
+// 	in_fd = STDIN_FILENO;
+// 	init_pids_array(pids, 1024);
+// 	while (node != NULL && num_cmds < 1024)
+// 	{
+// 		if (init_pipe_fds(pipe_fds, node) == -1)
+// 			break ;
+// 		pids[num_cmds] = fork();
+// 		if (pids[num_cmds] == -1)
+// 			break ;
+// 		if (pids[num_cmds] == 0)
+// 			exec_child_process(node, in_fd, env, pipe_fds);
+// 		else
+// 			manage_parent_process(&in_fd, pipe_fds, node);
+// 		tmp = node->next;
+// 		free_node(node);
+// 		node = tmp;
+// 		num_cmds++;
+// 	}
+// 	if (in_fd != -1)
+// 		close(in_fd);
+// 	wait_for_children(pids, num_cmds, child_status);
+// 	free_nodes(node);
+// }
